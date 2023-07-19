b928995b2953d0053c1656f74c5eb3743aafb4db
Reza <reza.fardi95@gmail.com>
2023-07-19 13:24:32 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x8521e7e97c496f1f0d1715d80c8cbc79582bb3ed";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
